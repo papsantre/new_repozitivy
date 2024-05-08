051a3023e2a704da41248e73f56ecc9a029b4f53
papsantre <egabundakov@mail.ru>
2024-05-08 23:43:06 +0500

    add 2 func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,16 @@
-a = str(input("Введите строку"))
-x = a.upper()
-print(x)
+def func_str():
+  """ функция перевода из нижнего резистра
+  в верхний регистр всех букв """
+  a = str(input("Введите строку"))
+  x = a.upper()
+  return (x)
+
+def func_strup():
+  """ функция перевода из нижнего резистра
+  в верхний регистр только первых букв"""
+  a = str(input("Введите строку"))
+  x = a.title()
+  return (x)
+
+print (func_str())
+print (func_strup())
