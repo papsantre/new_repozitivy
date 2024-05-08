f7245a6e103308359ebde09e87bd2734b234e2d6
papsantre <egabundakov@mail.ru>
2024-05-08 23:37:19 +0500

    Update str_func.py
    add docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
-a = str(input("Введите строку"))
-x = a.upper()
-print(x)
+def func_str():
+  """ функция перевода из нижнего резистра
+  в верхний регистр """
+  a = str(input("Введите строку"))
+  x = a.upper()
+  return (x)
+
+print (func_str())
